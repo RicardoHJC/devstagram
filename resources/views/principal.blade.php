0042e78ad6df728adb2e31eeaed14ef88159b322
Ricardo <ricardholgado@gmail.com>
2022-11-07 19:50:25 +0100

    Video 54, mi primer controller no funciona

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+      <h1>Página principal</h1>
+@endsection
+
+@section('contenido')
+       <h1>Este es el contenido de la pagina principal</h1>
+@endsection
