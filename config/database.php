bd913e2855cd9ecb760e99d8f8ec74f6347729af
Ricardo <ricardholgado@gmail.com>
2023-05-01 15:48:58 +0200

    acabada la validacion del formualrio y conectada con la base de datos. primeras migraciones de la base de datos OK, curso en el video 72

--- config/database.php
+++ config/database.php
@@ -43,6 +43,26 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'devstagram-mysql-1' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'mysql',
+            'username' => 'root',
+            'password' => '',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+    
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
